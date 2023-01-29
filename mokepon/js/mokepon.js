d491ffe42b7f484ba8bab8d253f533fa733053ea
mois3222 <moisky222@gmail.com>
2023-01-28 22:59:10 -0500

    commit: Ataques en JavaScript

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,6 +1,6 @@
 const d = document,
   inputPets = d.querySelectorAll(`input`),
-  PlayerPetbutton = document.querySelector("#buttonPets"),
+  PlayerPetbutton = document.querySelector(".buttonPets"),
   playersPet = d.getElementById("playersPet"),
   label = d.querySelectorAll(`label`),
   enemysPet = d.getElementById(`enemysPet`);
@@ -10,10 +10,18 @@ const random = (max, min) => {
 };
 
 d.addEventListener("DOMContentLoaded", (element) => {
-  PlayerPetbutton.addEventListener("click", (e) => {
-    inputPets.forEach((item) =>
-      item.checked ? (playersPet.textContent = item.id) : null
-    );
+  d.addEventListener("click", (e) => {
+    if (e.target.matches(".buttonPets"))
+      inputPets.forEach((item) =>
+        item.checked ? (playersPet.textContent = item.id) : null
+      );
+
+    if (e.target.matches(".buttonFire")) alert(`You attack is fire element 🔥`);
+
+    if (e.target.matches(".buttonAqua")) alert(`You attack is Aqua element💧`);
+
+    if (e.target.matches(".buttonTerra"))
+      alert(`You attack is Terra element🌱`);
   });
 
   try {
